312333b642871bf28dcd15fddbd88c78d3b46263
AhnYeonghoo <dksdudgn00@naver.com>
2023-09-19 06:39:45 +0900

    Promise function and Async Await

---
+++ 7.async.js
@@ -0,0 +1,37 @@
+function getBanana() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve('banana');
+		}, 1000);
+	});
+}
+
+function getApple() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve('apple');
+		}, 3000);
+	});
+}
+
+function getOrange() {
+	return Promise.reject(new Error('no orange'));
+}
+
+// 바나나와 사과를 같이 가지고 오기
+
+function fetchFruits() {
+	return getBanana().then((banana) =>
+		getApple().then((apple) => [banana, apple])
+	);
+}
+
+fetchFruits().then((fruits) => console.log(fruits));
+
+async function fetchAsyncFruits() {
+	const banana = await getBanana();
+	const apple = await getApple();
+	return [banana, apple];
+}
+
+fetchAsyncFruits().then((fruits) => console.log(fruits));
---
+++ MDNDocs/callApply.js
@@ -0,0 +1,21 @@
+function slow(x) {
+	console.log(`slow${x}를 호출함`);
+	return x;
+}
+
+function cachingDecorator(func) {
+	let cache = new Map();
+
+	return function (x) {
+		if (cache.has(x)) {
+			return cache.get(x);
+		}
+
+		let result = func(x);
+		cache.set(x, result);
+		return result;
+	};
+}
+
+slow = cachingDecorator(slow);
+console.log(slow(1));
