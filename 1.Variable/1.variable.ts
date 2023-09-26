6bf0823f60ebda6904f323f33c155a8bd773c48e
AhnYeonghoo <dksdudgn00@naver.com>
2023-09-26 10:39:30 +0900

    d

---
+++ 1.Variable/1.variable.ts
@@ -0,0 +1,8 @@
+let a: number = 0;
+console.log(a);
+
+a = 1;
+console.log(a);
+
+let b: number = 2;
+console.log(b);
---
+++ 1.Variable/2.naming.ts
@@ -0,0 +1,8 @@
+let apple: any;
+let redApple: any;
+let num: number = 20;
+let backgroundAudio: number;
+let windAudio: number;
+
+let audioBackground: string;
+let audioWind: string;
